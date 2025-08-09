04025b37ba493f546d538b48ccb6e99700bb373c
chris_p_mato <chris.y.patillo@gmail.com>
2025-08-08 20:03:05 -0400

    Sprite on Screen

--- Asteroids_Redux/main.py
+++ Asteroids_Redux/main.py
@@ -1,16 +1,23 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+   
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))  # Clear the screen with black
+        player.draw(screen)  # Draw the player
         pygame.display.flip()  # Update the display
+        dt = clock.tick(60) / 1000 # Limit to 60 FPS
+        
 
 if __name__ == "__main__":
     main()
