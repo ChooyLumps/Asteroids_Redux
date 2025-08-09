229db232392a0543863b575aba963f65c6757a95
chris_p_mato <chris.y.patillo@gmail.com>
2025-08-08 21:48:04 -0400

    Shooting Cooldown Implimented

--- Asteroids_Redux/player.py
+++ Asteroids_Redux/player.py
@@ -7,6 +7,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0  # Player's rotation angle
+        self.timer = 0  # Timer for shooting cooldown
 
     # in the player class
     def triangle(self):
@@ -39,8 +40,11 @@ class Player(CircleShape):
         if keys[pygame.K_s]:
             self.move(-dt)
         if keys[pygame.K_SPACE]:
-            self.shoot()
+            if self.timer <= 0:  # Check if the cooldown has passed
+                self.shoot()
+        self.timer -= dt
 
     def shoot(self):
         shot = Shot(self.position.x, self.position.y)
         shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+        self.timer = PLAYER_SHOOT_COOLDOWN  # Reset the cooldown timer
